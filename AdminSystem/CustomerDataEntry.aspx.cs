fa63a2c699b65b087d30a0173346022717f68e00
Joe-Smiton <P2724127@my365.dmu.ac.uk>
2024-05-20 18:50:06 +0100

    lab 8 is done again.

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -13,9 +13,35 @@ public partial class _1_DataEntry : System.Web.UI.Page
 
     }
 
-    protected void btnOK_Click(object sender, EventArgs e)
+    protected void btnOK_Click1(object sender, EventArgs e)
     {
 
+        //create a new instance of clsCustomer
+        clsCustomer Customer = new clsCustomer();
+        //capture the customer no
+        Customer.CustomerNo = Convert.ToInt32(txtCustomerNo.Text);
+        //capture the customer name
+        Customer.CustomerName = txtCustomerName.Text;
+        //capture the customer address
+        Customer.CustomerAddress = txtCustomerAddress.Text;
+        //capture the card no
+        Customer.CustomerCardNo = Convert.ToInt64(txtCustomerCardNo.Text);
+        //capture the card expiry
+        Customer.CustomerExpiryDate = Convert.ToDateTime(txtCustomerExpiryDate.Text);
+        //capture the card cvc
+        Customer.CustomerCVC = Convert.ToInt32(txtCustomerCVC.Text);
+        //capture the join date
+        Customer.CustomerJoinDate = Convert.ToDateTime(DateTime.Now);
+        //capture the account activity
+        Customer.AccountActivity = chkActive.Checked;
+        //store the name in the session object
+        Session["Customer"] = Customer;
+        //navigate to the view page
+        Response.Redirect("CustomerViewer.aspx");
+
+
     }
+
+
 
 }
