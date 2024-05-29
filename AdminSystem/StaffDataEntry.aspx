8c4bd25b99d01080e5f7dba809886b167711413b
Liibaan Ahmed <P2526198@my365.dmu.ac.uk>
2024-05-29 16:39:27 +0100

    labs 9-11

--- AdminSystem/StaffDataEntry.aspx
+++ AdminSystem/StaffDataEntry.aspx
@@ -6,10 +6,36 @@
 <head runat="server">
     <title></title>
 </head>
-<body>
+<body style>
     <form id="form1" runat="server">
         <div>
-        </div>
+       </div>
+        <asp:Label ID="lblStaffName" runat="server" style="z-index: 1; left: 23px; top: 80px; position: absolute" Text="Staff Name" height="19px" width="120px"></asp:Label>
+        <asp:Label ID="lblStaffPermission" runat="server" style="z-index: 1; left: 23px; top: 177px; position: absolute" Text="Permission" height="19px" width="120px"></asp:Label>
+        <asp:Label ID="lblStaffOnShift" runat="server" style="z-index: 1; left: 23px; top: 208px; position: absolute; margin-right: 1px;" Text="On Shift" height="19px" width="120px"></asp:Label>
+        <asp:TextBox ID="txtStaffName" runat="server" style="z-index: 1; left: 135px; top: 74px; position: absolute; height: 22px; width: 113px;"></asp:TextBox>
+        <asp:TextBox ID="txtStaffNo" runat="server" style="z-index: 1; left: 135px; top: 40px; position: absolute" height="22px" width="113px"></asp:TextBox>
+        <asp:TextBox ID="txtStaffRole" runat="server" style="z-index: 1; left: 135px; top: 108px; position: absolute" height="22px" width="113px"></asp:TextBox>
+        <asp:CheckBox ID="chkDataDelete" runat="server" height="19px" style="z-index: 1; left: 141px; top: 243px; position: absolute" Text="Delete" width="113px"/>
+        <asp:Label ID="lblError" runat="server" height="19px" style="z-index: 1; left: 17px; top: 302px; position: absolute" width="113px"></asp:Label>
+        <asp:Button ID="btnOK" runat="server" OnClick="btnOK_Click1" style="z-index: 1; left: 79px; top: 346px; position: absolute" Text="OK" />
+        <asp:Button ID="btnCancel" runat="server" style="z-index: 1; left: 190px; top: 348px; position: absolute" Text="Cancel" />
+        <asp:Label ID="lblStaffDataDelete" runat="server" style="z-index: 1; left: 23px; top: 244px; position: absolute; width: 120px; right: 788px;" Text="Delete Staff Data?" height="19px"></asp:Label>
+        <asp:Label ID="lblStaffNo" runat="server" style="z-index: 1; left: 23px; top: 44px; position: absolute; width: 120px;" Text="Staff No." height="19px"></asp:Label>
+        <p>
+        <asp:TextBox ID="txtStaffStart" runat="server" style="z-index: 1; left: 135px; top: 146px; position: absolute" height="22px" width="113px"></asp:TextBox>
+        </p>
+        <p>
+        <asp:Label ID="lblStaffStart" runat="server" style="z-index: 1; left: 23px; top: 148px; position: absolute" Text="Start Date" height="19px" width="120px"></asp:Label>
+        </p>
+        <p>
+        <asp:CheckBox ID="chkPermission" runat="server" height="19px" style="z-index: 1; left: 135px; top: 176px; position: absolute; margin-bottom: 0px;" Text="Yes" width="113px"/>
+        <asp:CheckBox ID="chkShift" runat="server" height="19px" style="z-index: 1; left: 135px; top: 206px; position: absolute; margin-bottom: 0px;" Text="Attendance" width="113px"/>
+        </p>
+        <p>
+        <asp:Label ID="lblStaffRole" runat="server" style="z-index: 1; left: 23px; top: 117px; position: absolute" Text="Staff Role" height="19px" width="120px"></asp:Label>
+        <asp:Button ID="btnFind" runat="server" OnClick="btnFind_Click" style="z-index: 1; left: 278px; top: 40px; position: absolute" Text="Find" />
+          <p>
     </form>
 </body>
 </html>
--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -4,13 +4,177 @@ namespace ClassLibrary
 {
     public class clsStaff
     {
-        public string StaffName { get; set; }
-        public bool StaffOnShift { get; set; }
-        public bool StaffPermission { get; set; }
-        public DateTime StaffStart { get; set; }
-        public string StaffRole { get; set; }
-        public bool StaffDataDelete { get; set; }
-        public int StaffNo { get; set; }
+        //private data member for the Staff number property
+        private Int32 mStaffNo;
+
+        //StaffNo public property
+        public int StaffNo
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffNo;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffNo = value;
+            }
+        }
+
+
+        //private data member for the Staff name property
+        private String mStaffName;
+
+        //StaffName public property
+        public String StaffName
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffName;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffName = value;
+            }
+        }
+
+
+        //private data member for the Staff Role property
+        private String mStaffRole;
+
+        //StaffRole public property
+        public String StaffRole
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffRole;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffRole = value;
+            }
+        }
+
+        //private data member for the Staff join date property
+        private DateTime mStaffStart;
+
+        //StaffStart public property
+        public DateTime StaffStart
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffStart;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffStart = value;
+            }
+        }
+
+
+        //private data member for the Staff Permission property
+        private Boolean mStaffPermission;
+
+        //StaffPermission public property
+        public bool StaffPermission
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffPermission;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffPermission = value;
+            }
+        }
+
+
+        //private data member for the Staff On Shift property
+        private Boolean mStaffOnShift;
+
+        //StaffPermission public property
+        public bool StaffOnShift
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffOnShift;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffOnShift = value;
+            }
+        }
+
+
+        //private data member for the Staff Data Delete property
+        private Boolean mStaffDataDelete;
+
+        //StaffPermission public property
+        public bool StaffDataDelete
+        {
+            get
+            {
+                //this line of code sends data out of the property
+                return mStaffDataDelete;
+            }
+            set
+            {
+                //this line of code allows data into the property
+                mStaffDataDelete = value;
+            }
+        }
+
+        public bool Active { get; set; }
+
+        public bool Find(int StaffNo)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameter for the address id to search for
+            DB.AddParameter("@StaffNo", StaffNo);
+            //execute the stored procedure
+            DB.Execute("sproc_tblStaff_FilterStaffNo");
+            //if one record is found (there should be either one or zero)
+            if (DB.Count == 1)
+            {
+
+
+                //copy the data from the database to the private data members
+                mStaffNo = Convert.ToInt32(DB.DataTable.Rows[0]["StaffNo"]);
+                mStaffPermission = Convert.ToBoolean(DB.DataTable.Rows[0]["StaffPermission"]);
+                mStaffOnShift = Convert.ToBoolean(DB.DataTable.Rows[0]["StaffOnShift"]);
+                mStaffDataDelete = Convert.ToBoolean(DB.DataTable.Rows[0]["StaffDataDelete"]);
+                mStaffStart = Convert.ToDateTime(DB.DataTable.Rows[0]["StaffStart"]);
+                mStaffRole = Convert.ToString(DB.DataTable.Rows[0]["StaffRole"]);
+                mStaffName = Convert.ToString(DB.DataTable.Rows[0]["StaffName"]);
+                //return that everything worked OK
+                return true;
+            }
+            //if no record was found
+            else
+            {
+                //return false indicating theres a problem
+                return false;
+            }
+        }
+
+
+        public string Valid(string staffName, string staffRole, string staffStart, string staffNo)
+
+        {
+            return "";
+        }
     }
 }
-    
+        
