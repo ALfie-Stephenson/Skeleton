8cc9d21b99d130eb5037a2f4b131ed98a029a041
Joe-Smiton <P2724127@my365.dmu.ac.uk>
2024-06-06 15:17:41 +0100

    lab 15 (accidentally mastered) all works fine

---
+++ ClassLibrary/clsCustomerUser.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class clsCustomerUser
+    {
+        //private data member for the userid property
+        private Int32 mUserID;
+        //private data member for the password property
+        private string mPassword;
+        //private data member for the username property
+        private string mUserName;
+        //private data member for the department property
+        private string mDepartment;
+        public int UserID
+        {
+            get
+            {
+                //return the priate data
+                return mUserID;
+            }
+            set
+            {
+                //set the private data
+                mUserID = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                //return the priate data
+                return mPassword;
+            }
+            set
+            {
+                //set the private data
+                mPassword = value;
+            }
+        }
+        public string UserName
+        {
+            get
+            {
+                //return the priate data
+                return mUserName;
+            }
+            set
+            {
+                //set the private data
+                mUserName = value;
+            }
+        }
+        public string Department
+        {
+            get
+            {
+                //return the priate data
+                return mDepartment;
+            }
+            set
+            {
+                //set the private data
+                mDepartment = value;
+            }
+        }
+
+        public bool FindUser(string UserName, string Password)
+        {
+            //create an instance of the data connection
+            clsDataConnection DB = new clsDataConnection();
+            //add the parameters for the username and password to search for
+            DB.AddParameter("@UserName", UserName);
+            DB.AddParameter("@Password", Password);
+            //execute the procedure
+            DB.Execute("sproc_tblUsers_FindUserNamePWCustomer");
+            //if one record is found (there should either be one or none)
+            if (DB.Count == 1)
+            {
+                //copy the data from the database to the private members
+                mUserID = Convert.ToInt32(DB.DataTable.Rows[0]["UserID"]);
+                mUserName = Convert.ToString(DB.DataTable.Rows[0]["UserName"]);
+                mPassword = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                mDepartment = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+                //return true to confirm everything worked ok
+                return true;
+            }
+            else
+            { 
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ Testing1/tstCustomerUser.cs
@@ -0,0 +1,112 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomerUser
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //test to see that it exists
+            Assert.IsNotNull(AnUser);
+        }
+
+        [TestMethod]
+        public void UserIDPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //create some test data to assign to the property
+            Int32 TestData = 1;
+            //assign the data to a property
+            AnUser.UserID = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AnUser.UserID, TestData);
+        }
+
+        [TestMethod]
+        public void UserNamePropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //create some test data to assign to the property
+            string TestData = "Joe";
+            //assign the data to a property
+            AnUser.UserName = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AnUser.UserName, TestData);
+        }
+
+        [TestMethod]
+        public void PasswordPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //create some test data to assign to the property
+            string TestData = "FrenchBoush";
+            //assign the data to a property
+            AnUser.Password = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AnUser.Password, TestData);
+        }
+
+        [TestMethod]
+        public void DepartmentPropertyOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //create some test data to assign to the property
+            string TestData = "Customer";
+            //assign the data to a property
+            AnUser.Department = TestData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(AnUser.Department, TestData);
+        }
+
+        [TestMethod]
+        public void FindUserMethodOK()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //create a boolean variable to store the results of the validation
+            Boolean Found = false;
+            //create some test data to use wiht the method
+            string UserName = "Joe";
+            string Password = "FrenchBoush";
+            //invoke the method
+            Found = AnUser.FindUser(UserName, Password);
+            //test to see if the result is true
+            Assert.IsTrue(Found);
+        }
+
+        [TestMethod]
+        public void TestUserNamePWFound()
+        {
+            //create an instance of the class we want to create
+            clsCustomerUser AnUser = new clsCustomerUser();
+            //create a boolean variable to store the results of the search
+            Boolean Found = false;
+            //create a boolean variable to record if the data is ok (assume it is)
+            Boolean OK = true;
+            //create some test data to use with the method
+            string UserName = "Joe";
+            string Password = "FrenchBoush";
+            //invoke the method
+            Found = AnUser.FindUser(UserName, Password);
+            //check the user id property
+            if (AnUser.UserName != UserName && AnUser.Password != Password)
+            {
+                OK = false;
+            }
+            //test to see that the result is correct
+            Assert.IsTrue(OK);
+        }
+
+
+    }
+}
